f4d94a44d2e5069a762b0e6779a83a0cfb85babb
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-08-16 21:35:02 -0300

    realice punto 1

--- js/app.js
+++ js/app.js
@@ -1,28 +1,53 @@
+// let botonComenzar = document.querySelector("#btnComenzar");
+// let formulario = document.querySelector("#formJuego")
+// let numeroMagico = 0;
+// //agregar eventos desde javascript
+// //botonComenzar.addEventListener("click", ()=>{comenzarJuego(variable)}); en el caso que tenga parametros mi funcion
+// botonComenzar.addEventListener("click", comenzarJuego);
+// formulario.addEventListener("submit", adivinarNumero);
+
+// function comenzarJuego(){
+//  numeroMagico = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+//  console.log(numeroMagico);
+// }
+
+// function adivinarNumero(e){
+//     e.preventDefault();
+//     console.log("desde la funcion adivinar numero");
+//     let numeroIngresado = document.querySelector("input").value;
+//     console.log(numeroIngresado);
+//     if(numeroIngresado = numeroMagico){
+//         alert("ganaste")
+//     }
+//     if(numeroIngresado > numeroMagico){
+//         alert("mm.. casi")
+//     }
+//     if(numeroIngresado < numeroMagico){
+//         alert("mm.. sigue intentando")
+//     }
+// }
+
 let botonComenzar = document.querySelector("#btnComenzar");
-let formulario = document.querySelector("#formJuego")
 let numeroMagico = 0;
-//agregar eventos desde javascript
-//botonComenzar.addEventListener("click", ()=>{comenzarJuego(variable)}); en el caso que tenga parametros mi funcion
+let formulario = document.querySelector("#formJuego");
 botonComenzar.addEventListener("click", comenzarJuego);
-formulario.addEventListener("submit", adivinarNumero);
+formulario.addEventListener("submit", adivinarNumeroMagico);
 
 function comenzarJuego(){
- numeroMagico = Math.floor(Math.random() * (10 - 1 + 1) + 1);
- console.log(numeroMagico);
+    numeroMagico = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+    console.log(numeroMagico);
 }
 
-function adivinarNumero(e){
+function adivinarNumeroMagico(e){
     e.preventDefault();
-    // console.log("desde la funcion adivinar numero");
     let numeroIngresado = document.querySelector("input").value;
-    console.log(numeroIngresado);
-    if(numeroIngresado = numeroMagico){
-        alert("ganaste")
+
+    if(numeroIngresado === numeroMagico){
+        alert("ganaste");
     }
-    if(numeroIngresado > numeroMagico){
-        alert("mm.. casi")
-    }
-    if(numeroIngresado < numeroMagico){
-        alert("mm.. sigue intentando")
+    else if(numeroIngresado > numeroMagico){
+        alert("el numero es menor");
+    }else if(numeroIngresado < numeroMagico){
+        alert("el numero es mayor");
     }
 }
