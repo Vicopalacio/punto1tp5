f911ab83f91f25ddf8eb5ba500d2d24a27c3464c
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-08-16 21:56:21 -0300

    borre comentarios

--- js/app.js
+++ js/app.js
@@ -1,32 +1,3 @@
-// let botonComenzar = document.querySelector("#btnComenzar");
-// let formulario = document.querySelector("#formJuego")
-// let numeroMagico = 0;
-// //agregar eventos desde javascript
-// //botonComenzar.addEventListener("click", ()=>{comenzarJuego(variable)}); en el caso que tenga parametros mi funcion
-// botonComenzar.addEventListener("click", comenzarJuego);
-// formulario.addEventListener("submit", adivinarNumero);
-
-// function comenzarJuego(){
-//  numeroMagico = Math.floor(Math.random() * (10 - 1 + 1) + 1);
-//  console.log(numeroMagico);
-// }
-
-// function adivinarNumero(e){
-//     e.preventDefault();
-//     console.log("desde la funcion adivinar numero");
-//     let numeroIngresado = document.querySelector("input").value;
-//     console.log(numeroIngresado);
-//     if(numeroIngresado = numeroMagico){
-//         alert("ganaste")
-//     }
-//     if(numeroIngresado > numeroMagico){
-//         alert("mm.. casi")
-//     }
-//     if(numeroIngresado < numeroMagico){
-//         alert("mm.. sigue intentando")
-//     }
-// }
-
 let botonComenzar = document.querySelector("#btnComenzar");
 let numeroMagico = 0;
 let formulario = document.querySelector("#formJuego");
